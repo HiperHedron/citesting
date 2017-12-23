f174a116dae93e6942195fdffeb60c228b207df6
HiperHedron <skaven1313>
2017-12-24 00:14:10 +0100

    1st

---
+++ src/tests/FirstTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	
+	WebDriver driver;
+	
+	@Test()
+	public void test() {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") +"/webdrivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		driver.quit();
+	}
+	
+	
+
+}
